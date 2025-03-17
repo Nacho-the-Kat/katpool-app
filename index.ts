319fe120fabd007d521a8ee7aeb3526dc34b193e
kr-kashyap <kashyap.n@knackroot.com>
2025-03-17 15:42:45 +0530

    Add check for canxium address.

--- index.ts
+++ index.ts
@@ -74,6 +74,13 @@ async function sendConfig() {
 const monitoring = new Monitoring();
 monitoring.log(`Main: Starting katpool App`)
 
+// Environment variable check
+const canxiumAddr = process.env.CANXIUM_ADDR;
+if (!canxiumAddr) {
+  throw new Error('Main: Environment variable CANXIUM_ADDR is not set.');
+}
+if (DEBUG) monitoring.debug(`Main: Obtained CANXIUM_ADDR : ${canxiumAddr}`);
+
 dotenv.config();
 
 monitoring.log(`Main: network: ${config.network}`);
