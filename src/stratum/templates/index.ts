b14f009a36fb47ffd8e57aec52a2303676fa316e
ktqappi <kt@alphaguilty.io>
2025-06-27 01:58:41 +0600

    fix: uncomment test net 10 bps

--- src/stratum/templates/index.ts
+++ src/stratum/templates/index.ts
@@ -94,104 +94,12 @@ export default class Templates {
     callback: (id: string, hash: string, timestamp: bigint, header: IRawHeader) => void
   ) {
     this.monitoring.log(`Templates ${this.port}: Registering new template callback`);
-    // this.rpc.addEventListener('new-block-template', async () => {
-    // const template = (await this.rpc.getBlockTemplate({
-    //   payAddress: this.address,
-    //   extraData: `${config.miner_info}`
-    // })).block as IRawBlock;
-
-    const templateChannel = config.redis_channel;
-    this.subscriber.subscribe(templateChannel, message => {
-      const fetchedTemplate = JSON.parse(message);
-      const blockTemplate = {
-        header: fetchedTemplate.Block.Header,
-        transactions: fetchedTemplate.Block.Transactions,
-      };
-      function convertJson(data: any) {
-        // Recursively traverse and transform keys
-        function transformKeys(obj: any): any {
-          if (Array.isArray(obj)) {
-            return obj.map((item: any) => transformKeys(item)); // Process arrays
-          } else if (obj !== null && typeof obj === 'object') {
-            let newObj: any = {};
-            for (const key in obj) {
-              if (obj.hasOwnProperty(key)) {
-                const newKey = key.toLowerCase(); // Convert key to lowercase
-                newObj[newKey] = transformKeys(obj[key]); // Recursively call for nested objects
-              }
-            }
-            return newObj;
-          }
-          return obj; // Return the value if it's neither an array nor an object
-        }
-
-        // First, transform all keys to lowercase
-        const transformedKeysData = transformKeys(data);
-        const parents = transformedKeysData.header.parents;
-        delete transformedKeysData.header.parents;
-
-        let parentsByLevel: any[] = [];
-        parents.map((item: any, i: number) => {
-          parentsByLevel[i] = item.parenthashes;
-        });
-
-        transformedKeysData.header['parentsByLevel'] = parentsByLevel;
-
-        return transformedKeysData;
-      }
-      const converted = convertJson(blockTemplate);
-      const tHeader: IRawHeader = {
-        version: converted.header.version,
-        parentsByLevel: converted.header.parentsByLevel,
-        hashMerkleRoot: converted.header.hashmerkleroot,
-        acceptedIdMerkleRoot: converted.header.acceptedidmerkleroot,
-        utxoCommitment: converted.header.utxocommitment,
-        timestamp: BigInt(converted.header.timestamp),
-        bits: converted.header.bits,
-        nonce: BigInt(converted.header.nonce),
-        daaScore: BigInt(converted.header.daascore),
-        blueWork: converted.header.bluework,
-        blueScore: BigInt(converted.header.bluescore),
-        pruningPoint: converted.header.pruningpoint,
-      };
-      const tTransactions = converted.transactions.map((tx: any) => ({
-        version: tx.version,
-        inputs:
-          tx.inputs.length > 0
-            ? tx.inputs.map((inputItem: any) => ({
-                previousOutpoint: {
-                  transactionId: inputItem.previousoutpoint.transactionid,
-                  index: inputItem.previousoutpoint.index,
-                },
-                signatureScript: inputItem.signaturescript,
-                sequence: BigInt(inputItem.sequence),
-                sigOpCount: inputItem.sigopcount,
-                verboseData: inputItem.verbosedata,
-              }))
-            : tx.inputs,
-        outputs:
-          tx.outputs.length > 0
-            ? tx.outputs.map((outputItem: any) => ({
-                value: BigInt(outputItem.amount),
-                scriptPublicKey: outputItem.scriptpublickey,
-                verboseData: outputItem.verbosedata,
-              }))
-            : tx.outputs,
-        lockTime: BigInt(tx.locktime),
-        subnetworkId: tx.subnetworkid,
-        gas: BigInt(tx.gas),
-        payload: tx.payload,
-        mass: BigInt(tx.mass || 0),
-        verboseData: tx.verbosedata,
-      }));
-      const template = {
-        header: tHeader,
-        transactions: tTransactions,
-      };
-      if ((template.header.blueWork as string).length % 2 !== 0) {
-        template.header.blueWork = '0' + template.header.blueWork;
-      }
-
+    // --- Begin: Default template fetching logic ---
+    this.rpc.addEventListener('new-block-template', async () => {
+      const template = (await this.rpc.getBlockTemplate({
+        payAddress: this.address,
+        extraData: `${config.miner_info}`
+      })).block as any; // TODO: Replace 'any' with the correct type (IRawBlock) when available
       const header = new Header(template.header);
       const headerHash = header.finalize();
 
@@ -211,7 +119,16 @@ export default class Templates {
 
       callback(id, proofOfWork.prePoWHash, header.timestamp, template.header);
     });
-    // })
+    // --- End: Default template fetching logic ---
+
+    /*
+    // --- Begin: Redis-based template subscription (disabled) ---
+    const templateChannel = config.redis_channel;
+    this.subscriber.subscribe(templateChannel, message => {
+      // ... Redis template logic ...
+    });
+    // --- End: Redis-based template subscription (disabled) ---
+    */
 
     await this.rpc.subscribeNewBlockTemplate();
   }
